ea21e9981f42a71060fe277fd073f623e2d0dd9d
Christof Vollrath <christof@taobits.net>
2018-06-29 15:54:47 +0200

    Day 20 Part 1

--- src/test/kotlin/Day20.kt
+++ src/test/kotlin/Day20.kt
@@ -3,6 +3,8 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
+import java.lang.Long.max
+import java.lang.Long.min
 
 /*
 --- Day 20: Firewall Rules ---
@@ -27,19 +29,148 @@ Then, the only IPs that this firewall allows are 3 and 9, since those are the on
 Given the list of blocked IPs you retrieved from the firewall (your puzzle input),
 what is the lowest-valued IP that is not blocked?
 
+--- Part Two ---
+
+How many IPs are allowed by the blacklist?
+
  */
+
+fun parseIpRanges(lines: List<String>): List<IpRange> =
+        lines.map {
+            val parts = it.split("-")
+            IpRange(parts[0].toLong(), parts[1].toLong())
+        }
+
+typealias IpRange = Pair<Long, Long>
+
+fun mergeRanges(input: List<IpRange>): List<IpRange> {
+    val sorted = input.sortedBy { it.first }
+    // Sorting makes sure that after a merge not additional merges must be considered, e.g (1,2), (5,6), (3,4)
+    // after merging (1,2) with (3,4) to (1,4) it could be merged to (1,6)
+
+    var result = listOf<IpRange>()
+    sorted.forEach { newIpRange ->
+        val modifiedResult = mutableListOf<IpRange>()
+        var merged = false
+        result.forEach { existingIpRange ->
+            if (!merged && overlapping(existingIpRange, newIpRange)) {
+                modifiedResult.add(mergeIpRange(newIpRange, existingIpRange))
+                merged = true
+            } else modifiedResult.add(existingIpRange)
+        }
+        if (! merged) modifiedResult.add(newIpRange)
+        result = modifiedResult
+    }
+    return result.toList()
+}
+
+fun mergeIpRange(newIpRange: IpRange, existingIpRange: IpRange) =
+        IpRange(min(newIpRange.first, existingIpRange.first), max(newIpRange.second, existingIpRange.second))
+
+
+fun overlapping(ipRange1: IpRange, ipRange2: IpRange) =
+        (ipRange1.first <= ipRange2.first && ipRange1.second >= ipRange2.first - 1) ||
+                (ipRange2.first <= ipRange1.first && ipRange2.second >= ipRange1.first - 1)
+
+fun lowestValidIp(input: List<IpRange>): Long {
+    val firstRange = input.minBy { it.first }
+    if (firstRange != null && firstRange.first <= 0) return firstRange.second + 1L
+    else return 0L
+}
+
 
 object Day20Spec : Spek({
 
     describe("part 1") {
         given("example") {
-            val input = listOf(Pair(5, 8), Pair(0, 2), Pair(4,7))
-            it("should find correct elf") {
-                lowestValidIp(input, 9) `should equal` 3
+            val input = listOf(IpRange(5, 8), IpRange(0, 2), IpRange(4,7))
+            it("should find correct lowest value") {
+                lowestValidIp(mergeRanges(input)) `should equal` 3
+            }
+        }
+        describe("merge ranges") {
+            given("an empty list") {
+                val emptyList = listOf<IpRange>()
+                it("should return the empty list") {
+                    mergeRanges(emptyList) `should equal` listOf()
+                }
+            }
+            given("a list with one element") {
+                val emptyList = listOf<IpRange>(IpRange(1, 2))
+                it("should return the list with one element") {
+                    mergeRanges(emptyList) `should equal` listOf(IpRange(1, 2))
+                }
+            }
+            given("a list with non overlapping elements") {
+                val emptyList = listOf<IpRange>(IpRange(1, 2), IpRange(4, 6))
+                it("should return the list with these elements") {
+                    mergeRanges(emptyList) `should equal` listOf(IpRange(1, 2), IpRange(4, 6))
+                }
+            }
+            given("a list with overlapping elements") {
+                val emptyList = listOf<IpRange>(IpRange(1, 5), IpRange(4, 6))
+                it("should return a list with one merged element") {
+                    mergeRanges(emptyList) `should equal` listOf(IpRange(1, 6))
+                }
+            }
+            given("a list with overlapping elements, different order") {
+                val emptyList = listOf<IpRange>(IpRange(4, 6), IpRange(1, 5))
+                it("should return a list with one merged element") {
+                    mergeRanges(emptyList) `should equal` listOf(IpRange(1, 6))
+                }
+            }
+            given("a list with nearby elements") {
+                val emptyList = listOf<IpRange>(IpRange(1, 3), IpRange(4, 7))
+                it("should return a list with one merged element") {
+                    mergeRanges(emptyList) `should equal` listOf(IpRange(1, 7))
+                }
+            }
+            given("a list with nearby elements, different order") {
+                val emptyList = listOf<IpRange>(IpRange(4, 7), IpRange(1, 3))
+                it("should return a list with one merged element") {
+                    mergeRanges(emptyList) `should equal` listOf(IpRange(1, 7))
+                }
+            }
+            given("example") {
+                val input = listOf(IpRange(5, 8), IpRange(0, 2), IpRange(4, 7))
+                it("should merge to correct list") {
+                    mergeRanges(input).toSet() `should equal` listOf(IpRange(0, 2), IpRange(4, 8)).toSet()
+                }
+            }
+            given("bugs") {
+                val input = listOf(IpRange(868214, 1216244), IpRange(591078, 868213))
+                it("should merge to correct list") {
+                    mergeRanges(input) `should equal` listOf(IpRange(591078, 1216244))
+                }
+            }
+        }
+        given("exercise") {
+            val inputString = readTrimedLinesFromResource("day20Input.txt")
+            val input = parseIpRanges(inputString)
+            it("should find correct lowest value") {
+                lowestValidIp(mergeRanges(input)) `should equal` 4793564
             }
         }
     }
+
+    describe("part 2") {
+        given("example") {
+            val input = listOf(IpRange(5, 8), IpRange(0, 2), IpRange(4, 7))
+            it("should find all allowed ips") {
+                findValidIps(mergeRanges(input), 9) `should equal` listOf(3, 9)
+            }
+        }
+        given("exercise") {
+            val inputString = readTrimedLinesFromResource("day20Input.txt")
+            val input = parseIpRanges(inputString)
+            TODO()
+        }
+    }
+
 })
 
-fun lowestValidIp(input: List<Pair<Int, Int>>, max: Int) = 3
+fun findValidIps(ranges: List<IpRange>, n: Long) =
+        (0..n).filter { checkIp(it, ranges) }
 
+fun checkIp(ip: Long, ranges: List<IpRange>) =
+        ! ranges.any { ip >= it.first && ip <= it.second }
