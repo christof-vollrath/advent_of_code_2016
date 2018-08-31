e01dea29922aeee1d9294842229e98b337ce8b02
Christof Vollrath <christof.vollrath@spring-media.de>
2018-08-31 16:17:13 +0200

    Day 24 Part 1 started connection locations

--- src/test/kotlin/Day24.kt
+++ src/test/kotlin/Day24.kt
@@ -47,8 +47,73 @@ required to visit every non-0 number marked on the map at least once?
 
 */
 
-data class AirDuctLocation(val pos: Pair<Int, Int>, val number: Int? = null)
-typealias AirDuctGraph = Set<AirDuctLocation>
+typealias Pos = Pair<Int, Int>
+
+data class Connection(val location: AirDuctLocation, val distance: Int)
+
+data class AirDuctLocation(val pos: Pair<Int, Int>, val number: Int? = null) {
+    lateinit var connections: Set<Connection>
+}
+
+class AirDuctGraph(
+        val locations: Set<AirDuctLocation>,
+        val locationArray: Array<Array<Char>>,
+        val locationByPos: Map<Pos, AirDuctLocation> = initLocationMap(locations)
+    ) : Set<AirDuctLocation> by locations {
+        init {
+            locations.forEach {
+                it.connections = findDirectConnections(it, this)
+            }
+        }
+}
+
+fun findDirectConnections(location: AirDuctLocation, graph: AirDuctGraph) =
+        when {
+            location.pos == Pair(1, 1) && graph.locationByPos[Pair(2, 1)] != null -> setOf(Connection(graph.locationByPos[Pair(2, 1)]!!, 0))
+            location.pos == Pair(2, 1) && graph.locationByPos[Pair(1, 1)] != null -> setOf(Connection(graph.locationByPos[Pair(1, 1)]!!, 0))
+            else -> emptySet()
+        }
+
+fun initLocationMap(locations: Set<AirDuctLocation>): Map<Pos, AirDuctLocation> = locations.map { Pair(it.pos, it) }.toMap()
+
+fun parseAirDuctMap(input: String): AirDuctGraph = buildAirDuctMapFromArray(parseAirDuctMapToArray(input))
+
+fun buildAirDuctMapFromArray(array: Array<Array<Char>>): AirDuctGraph = AirDuctGraph(
+        buildSequence {
+            array.forEachIndexed { y, line ->
+                line.forEachIndexed { x, c ->
+                    when {
+                        c.isDigit() -> yield(AirDuctLocation(Pair(x, y), c.toString().toInt()))
+                        c == '.' && isJunction(x, y, array) -> yield(AirDuctLocation(Pair(x, y)))
+                    }
+                }
+            }
+        }.toSet(),
+        array
+    )
+
+fun parseAirDuctMapToArray(input: String): Array<Array<Char>> = with(parseTrimedLines(input)) {
+    Array<Array<Char>>(size) {y ->
+        val line = this[y]
+        Array<Char>(line.length) { x ->
+            line[x]
+        }
+    }
+}
+
+fun isJunction(x: Int, y: Int, array: Array<Array<Char>>) =
+        adjacentPositions(x, y, array).count() > 2 // Junction has more than 2 ways leading to it
+
+fun adjacentPositions(x: Int, y: Int, array: Array<Array<Char>>): Set<Pos> =
+    listOf(Pair(1, 0), Pair(0, 1), Pair(-1, 0), Pair(0, -1)).map {
+        Pair(x + it.first, y + it.second)
+    }
+    .filter {
+        it.second > 0 && it.second < array.size && it.first > 0 && it.first < array[it.second].size && array[it.second][it.first] != '#'
+    }
+    .toSet()
+
+fun adjacentPositions(pos: Pos, graph: AirDuctGraph): Set<Pos> = adjacentPositions(pos.first, pos.second, graph.locationArray)
 
 class Day24Spec: Spek({
     describe("part 1") {
@@ -110,37 +175,45 @@ class Day24Spec: Spek({
                     parseAirDuctMap(input) `should equal` setOf(AirDuctLocation(Pair(2, 1)))
                 }
             }
+            given("input map with two locations") {
+                val input = """
+                    #####
+                    #0#1#
+                    #####
+                """.trimIndent()
+                it("should be parsed to graph with these locations ") {
+                    parseAirDuctMap(input) `should equal` setOf(AirDuctLocation(Pair(1, 1), 0), AirDuctLocation(Pair(3, 1), 1))
+                }
+            }
+            given("input map with two adjacent locations") {
+                val input = """
+                    ####
+                    #01#
+                    ####
+                """.trimIndent()
+                it("should be parsed to graph with these locations ") {
+                    val airDuctGraph = parseAirDuctMap(input)
+                    airDuctGraph `should equal` setOf(AirDuctLocation(Pair(1, 1), 0), AirDuctLocation(Pair(2, 1), 1))
+                    airDuctGraph.locationByPos[Pair(1, 1)]!!.connections `should equal` setOf(Connection(airDuctGraph.locationByPos[Pair(2, 1)]!!,  0))
+                    airDuctGraph.locationByPos[Pair(2, 1)]!!.connections `should equal` setOf(Connection(airDuctGraph.locationByPos[Pair(1, 1)]!!, 0))
+                }
+            }
+        }
+        describe("adjacentPositions") {
+            given("input map with a junction and some numbers") {
+                val input = """
+                    #####
+                    #0..#
+                    ##1##
+                """.trimIndent()
+                val graph = parseAirDuctMap(input)
+                it("should find adjacent positions") {
+                     adjacentPositions(Pair(2, 1), graph) `should equal` setOf(Pair(1, 1), Pair(2, 2), Pair(3, 1))
+                }
+            }
         }
     }
 })
 
-fun parseAirDuctMap(input: String): AirDuctGraph = buildAirDuctMapFromArray(parseAirDuctMapToArray(input))
-
-fun buildAirDuctMapFromArray(array: Array<Array<Char>>): AirDuctGraph = buildSequence {
-    array.forEachIndexed { y, line ->
-        line.forEachIndexed { x, c ->
-            when {
-                c.isDigit() -> yield(AirDuctLocation(Pair(x, y), c.toString().toInt()))
-                c == '.' && isJunction(x, y, array) -> yield(AirDuctLocation(Pair(x, y)))
-            }
-        }
-    }
-}.toSet()
-
-fun parseAirDuctMapToArray(input: String): Array<Array<Char>> = with(parseTrimedLines(input)) {
-    Array<Array<Char>>(size) {y ->
-        val line = this[y]
-        Array<Char>(line.length) { x ->
-            line[x]
-        }
-    }
-}
-
-fun isJunction(x: Int, y: Int, array: Array<Array<Char>>) =
-    listOf(Pair(1, 0), Pair(0, 1), Pair(-1, 0), Pair(0, -1)).filter {
-        val x2 = x + it.first
-        val y2 = y + it.second
-        y2 > 0 && y2 < array.size && x2 > 0 && x2 < array[y2].size &&array[y + it.second][x + it.first] == '.'
-    }.count() > 2 // Junction has more than 2 ways leading to it
 
 
