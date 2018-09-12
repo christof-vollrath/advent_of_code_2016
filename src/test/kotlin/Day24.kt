db2666952d86f54edd425cd30c97380a7df4d9d3
Christof Vollrath <christof.vollrath@spring-media.de>
2018-09-12 15:14:49 +0200

    Day 24 Part 1 finding shortest path works but breadth first search consumes too much memory for exercise

--- src/test/kotlin/Day24.kt
+++ src/test/kotlin/Day24.kt
@@ -58,7 +58,8 @@ data class AirDuctLocation(val pos: Pair<Int, Int>, val number: Int? = null) {
 class AirDuctGraph(
         val locations: Set<AirDuctLocation>,
         val locationArray: Array<Array<Char>>,
-        val locationByPos: Map<Pos, AirDuctLocation> = initLocationMap(locations)
+        val locationByPos: Map<Pos, AirDuctLocation> = initLocationMap(locations),
+        val locationByNumber: Map<Int, AirDuctLocation> = initLocationMapByNumber(locations)
     ) : Set<AirDuctLocation> by locations {
         init {
             locations.forEach {
@@ -69,7 +70,7 @@ class AirDuctGraph(
 
 data class AirDuctPathElement(val location: AirDuctLocation, val distance: Int = 0)
 
-typealias AirDuctPath = List<AirDuctPathElement>
+data class AirDuctPath(val path: List<AirDuctPathElement>, val length: Int)
 
 fun findConnections(location: AirDuctLocation, graph: AirDuctGraph) =
         adjacentPositions(location.pos, graph)
@@ -96,6 +97,12 @@ fun followConnection(graph: AirDuctGraph, pos: Pos, from: Pos): Connection? {
 
 
 fun initLocationMap(locations: Set<AirDuctLocation>): Map<Pos, AirDuctLocation> = locations.map { Pair(it.pos, it) }.toMap()
+fun initLocationMapByNumber(locations: Set<AirDuctLocation>): Map<Int, AirDuctLocation> = locations.mapNotNull {
+    if (it.number != null)
+        Pair(it.number, it)
+    else
+        null
+}.toMap()
 
 fun parseAirDuctMap(input: String): AirDuctGraph = buildAirDuctMapFromArray(parseAirDuctMapToArray(input))
 
@@ -136,8 +143,54 @@ fun adjacentPositions(x: Int, y: Int, array: Array<Array<Char>>): Set<Pos> =
 
 fun adjacentPositions(pos: Pos, graph: AirDuctGraph): Set<Pos> = adjacentPositions(pos.first, pos.second, graph.locationArray)
 
-fun findShortestPath(airDuctGraph: AirDuctGraph): Any {
-    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+fun findShortestPath(airDuctGraph: AirDuctGraph): AirDuctPath {
+    val start = airDuctGraph.locationByNumber[0] ?: throw IllegalStateException("No starting node found")
+    val nodesToVisit = airDuctGraph.locations.mapNotNull { it.number } - 0
+    val startPath = AirDuctPath(listOf(AirDuctPathElement(start,0)), 0)
+    var searches = listOf(Pair(nodesToVisit - 0, startPath))
+    return findShortestPath(searches)
+}
+
+tailrec fun findShortestPath(searches: List<Pair<List<Int>, AirDuctPath>>): AirDuctPath {
+    fun expandPath(remainingNodes: List<Int>, path: AirDuctPath): List<Pair<List<Int>, AirDuctPath>> {
+        val lastLocationInPath = path.path.last().location
+        val prevLocationInPath = if (path.path.size > 1) path.path[path.path.size - 2]
+        else null
+        val connections = lastLocationInPath.connections
+        val minimizedConnections = if (lastLocationInPath.number != null || prevLocationInPath == null) connections // for number nodes going back might be needed (in the example the path is 0 - 4 - 0
+        else connections.filter { it.location != prevLocationInPath.location } // but for junctions it doesn't make sense
+        return minimizedConnections.map {connection ->
+            val nextRemainingNodes =
+                    if (connection.location.number != null) remainingNodes - connection.location.number
+                    else remainingNodes
+            val nextPath = AirDuctPath(path.path + AirDuctPathElement(connection.location, connection.distance), path.length + connection.distance)
+            Pair(nextRemainingNodes, nextPath)
+        }
+    }
+    fun purgeSearches(searches: List<Pair<List<Int>, AirDuctPath>>): List<Pair<List<Int>, AirDuctPath>> {
+        val completePathes = searches.filter { it.first.size == 0 }
+        val shortestSolution = completePathes
+                .map { it.second }
+                .minBy { it.length }
+        val shortestSolutionLength = shortestSolution?.length ?: Int.MAX_VALUE
+        val purgedSearches = searches.filter {
+            it.first.size == 0 || it.second.length < shortestSolutionLength
+        }
+        println("Interims=${searches.size} Completed=${completePathes.size} Purged=${purgedSearches.size}")
+        return purgedSearches
+    }
+    val moreToCheck = searches.any { it.first.size > 0}
+    if (! moreToCheck) {
+        return searches.map { it.second }.minBy { it.length }!!
+    } else {
+        val nextSearches = searches.flatMap {
+            val remainingNodes = it.first
+            val path = it.second
+            if (remainingNodes.size == 0) listOf(it)
+            else expandPath(remainingNodes, path)
+        }
+        return findShortestPath(purgeSearches(nextSearches))
+    }
 }
 
 class Day24Spec: Spek({
@@ -338,31 +391,6 @@ class Day24Spec: Spek({
 
             }
         }
-        describe("shortest path") {
-            given("example input map") {
-                val input = """
-                    ###########
-                    #0.1.....2#
-                    #.#######.#
-                    #4.......3#
-                    ###########
-                """.trimIndent()
-                val airDuctGraph = parseAirDuctMap(input)
-
-                it("should find the shortest path") {
-                    val airDuctPath = findShortestPath(airDuctGraph)
-                    airDuctPath `should equal` listOf(
-                            AirDuctPathElement(AirDuctLocation(Pair(1, 1), 0), 0),
-                            AirDuctPathElement(AirDuctLocation(Pair(1, 3), 4), 2),
-                            AirDuctPathElement(AirDuctLocation(Pair(1, 1), 0), 2),
-                            AirDuctPathElement(AirDuctLocation(Pair(3, 1), 1), 2),
-                            AirDuctPathElement(AirDuctLocation(Pair(9, 1), 2), 6),
-                            AirDuctPathElement(AirDuctLocation(Pair(9, 3), 3), 2)
-                    )
-                }
-            }
-
-        }
         describe("adjacentPositions") {
             given("input map with a junction and some numbers") {
                 val input = """
@@ -373,7 +401,90 @@ class Day24Spec: Spek({
                 """.trimIndent()
                 val graph = parseAirDuctMap(input)
                 it("should find adjacent positions") {
-                     adjacentPositions(Pair(2, 1), graph) `should equal` setOf(Pair(1, 1), Pair(2, 2), Pair(3, 1))
+                    adjacentPositions(Pair(2, 1), graph) `should equal` setOf(Pair(1, 1), Pair(2, 2), Pair(3, 1))
+                }
+            }
+        }
+        describe("shortest path") {
+            given("a map with only the starting node") {
+                val input = """
+                    ###########
+                    #0........#
+                    ###########
+                """.trimIndent()
+                val airDuctGraph = parseAirDuctMap(input)
+
+                it("should find a path with only the starting node") {
+                    val airDuctPath = findShortestPath(airDuctGraph)
+                    airDuctPath `should equal` AirDuctPath(
+                            listOf(
+                                AirDuctPathElement(AirDuctLocation(Pair(1, 1), 0), 0)
+                            ),
+                            0
+                    )
+                }
+            }
+            given("a map with two nodes") {
+                val input = """
+                    ###########
+                    #0.......1#
+                    ###########
+                """.trimIndent()
+                val airDuctGraph = parseAirDuctMap(input)
+
+                it("should find a path with only the starting node") {
+                    val airDuctPath = findShortestPath(airDuctGraph)
+                    airDuctPath `should equal`  AirDuctPath(
+                            listOf(
+                                AirDuctPathElement(AirDuctLocation(Pair(1, 1), 0), 0),
+                                AirDuctPathElement(AirDuctLocation(Pair(9, 1), 1), 8)
+                            ),
+                            8
+                    )
+                }
+            }
+            given("example input map") {
+                val input = """
+                    ###########
+                    #0.1.....2#
+                    #.#######.#
+                    #4.......3#
+                    ###########
+                """.trimIndent()
+                val airDuctGraph = parseAirDuctMap(input)
+
+                    it("should find the shortest path") {
+                    val airDuctPath = findShortestPath(airDuctGraph)
+                    airDuctPath `should equal` AirDuctPath(
+                            listOf(
+                                AirDuctPathElement(AirDuctLocation(Pair(1, 1), 0), 0),
+                                AirDuctPathElement(AirDuctLocation(Pair(1, 3), 4), 2),
+                                AirDuctPathElement(AirDuctLocation(Pair(1, 1), 0), 2),
+                                AirDuctPathElement(AirDuctLocation(Pair(3, 1), 1), 2),
+                                AirDuctPathElement(AirDuctLocation(Pair(9, 1), 2), 6),
+                                AirDuctPathElement(AirDuctLocation(Pair(9, 3), 3), 2)
+                            ),
+                            14
+                    )
+                }
+            }
+            given("exercise input map") {
+                val input = readResource("day24Input.txt")
+                val airDuctGraph = parseAirDuctMap(input)
+
+                it("should find the shortest path") {
+                    val airDuctPath = findShortestPath(airDuctGraph)
+                    airDuctPath `should equal` AirDuctPath(
+                            listOf(
+                                    AirDuctPathElement(AirDuctLocation(Pair(1, 1), 0), 0),
+                                    AirDuctPathElement(AirDuctLocation(Pair(1, 3), 4), 2),
+                                    AirDuctPathElement(AirDuctLocation(Pair(1, 1), 0), 2),
+                                    AirDuctPathElement(AirDuctLocation(Pair(3, 1), 1), 2),
+                                    AirDuctPathElement(AirDuctLocation(Pair(9, 1), 2), 6),
+                                    AirDuctPathElement(AirDuctLocation(Pair(9, 3), 3), 2)
+                            ),
+                            14
+                    )
                 }
             }
         }
